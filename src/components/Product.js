31ff4525c92b8462dd1bdbd93850eb60fc642b29
Saadat Badgujar <saadatbadgujar@gmail.com>
2023-03-06 20:14:53 +0530

    removed react-currency-formatter

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { StarIcon } from "@heroicons/react/24/solid";
-import Currency from "react-currency-formatter";
+// import Currency from "react-currency-formatter";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
@@ -35,7 +35,8 @@ const Product = ({ id, title, price, description, category, image }) => {
 
       <p className="text-xs my-2 line-clamp-2">{description}</p>
       <div className="mb-5">
-        <Currency quantity={price} currency="INR" decimal="" />
+        {/* <Currency quantity={price} currency="INR" decimal="" /> */}
+        {"$" + price}
       </div>
       {hasPrime && (
         <div className="flex items-center space-x-2 -mt-5">

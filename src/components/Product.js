28fa59ec92e7ae95a4b318ed66d6ed847e9fd412
Saadat Badgujar <saadatbadgujar@gmail.com>
2023-03-06 13:39:21 +0530

    Add ad banner and changed layout of productfeed

--- src/components/Product.js
+++ src/components/Product.js
@@ -38,7 +38,7 @@ const Product = ({ id, title, price, description, category, image }) => {
         <Currency quantity={price} currency="INR" decimal="" />
       </div>
       {hasPrime && (
-        <div className="flex items-center space-x-2 mt-5">
+        <div className="flex items-center space-x-2 -mt-5">
           <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
           <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
